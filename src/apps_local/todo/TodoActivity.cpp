89e00a76c0e111074d6e563def137462564e3e92
MinaBikhit <minaabbmorgan@gmail.com>
2026-09-13 11:07:09 +0200

    Add Todo app shell

--- src/apps_local/todo/TodoActivity.cpp
+++ src/apps_local/todo/TodoActivity.cpp
@@ -1,4 +1,4 @@
-#include "SampleActivity.h"
+#include "TodoActivity.h"
 
 #include <Memory.h>
 
@@ -6,39 +6,62 @@
 #include "../Shelf.h"
 #include "fontIds.h"
 
-std::unique_ptr<Activity> SampleActivity::create(GfxRenderer& renderer, MappedInputManager& mappedInput) {
-  return makeUniqueNoThrow<SampleActivity>(renderer, mappedInput);
+std::unique_ptr<Activity> TodoActivity::create(
+    GfxRenderer& renderer,
+    MappedInputManager& mappedInput) {
+  return makeUniqueNoThrow<TodoActivity>(renderer, mappedInput);
 }
 
-void SampleActivity::onEnter() {
+void TodoActivity::onEnter() {
   Activity::onEnter();
   requestUpdate();
 }
 
-void SampleActivity::loop() {
-  // An app never names where Back goes; the shelf puts it back in whichever
-  // folder opened it. See docs/shelf.md. The global back-swipe gesture arrives
-  // as Button::Back too.
+void TodoActivity::loop() {
   if (mappedInput.wasReleased(MappedInputManager::Button::Back)) {
     shelf::leave(renderer, mappedInput);
   }
 }
 
-void SampleActivity::render(RenderLock&&) {
+void TodoActivity::render(RenderLock&&) {
   const auto& metrics = UITheme::getInstance().getMetrics();
   const int sw = renderer.getScreenWidth();
   const int sh = renderer.getScreenHeight();
 
   renderer.clearScreen();
-  GUI.drawHeader(renderer, Rect{0, metrics.topPadding, sw, metrics.headerHeight}, "Sample");
 
-  const int bodyY = metrics.topPadding + metrics.headerHeight + metrics.verticalSpacing;
-  const int bodyH = sh - bodyY - metrics.buttonHintsHeight - metrics.verticalSpacing;
-  UITheme::drawCenteredWrappedText(renderer, Rect{0, bodyY, sw, bodyH}, UI_12_FONT_ID,
-                                   "Local app seam works. Copy src/apps_local/sample/ to start a real one.", 4);
+  GUI.drawHeader(
+      renderer,
+      Rect{0, metrics.topPadding, sw, metrics.headerHeight},
+      "TO DO");
 
-  const auto labels = mappedInput.mapLabels("Back", "", "", "");
-  GUI.drawButtonHints(renderer, labels.btn1, labels.btn2, labels.btn3, labels.btn4);
+  const int bodyY =
+      metrics.topPadding +
+      metrics.headerHeight +
+      metrics.verticalSpacing;
+
+  const int bodyH =
+      sh -
+      bodyY -
+      metrics.buttonHintsHeight -
+      metrics.verticalSpacing;
+
+  UITheme::drawCenteredWrappedText(
+      renderer,
+      Rect{0, bodyY, sw, bodyH},
+      UI_12_FONT_ID,
+      "Todo app is working!",
+      4);
+
+  const auto labels =
+      mappedInput.mapLabels("Back", "", "", "");
+
+  GUI.drawButtonHints(
+      renderer,
+      labels.btn1,
+      labels.btn2,
+      labels.btn3,
+      labels.btn4);
 
   renderer.displayBuffer();
 }
